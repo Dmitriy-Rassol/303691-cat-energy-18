eb53910ac3d4ff9eb103e8a2bd88f7b711af4cfe
Dmitriy-Rassol <rassoldima@yandex.ru>
2020-01-08 03:11:13 +0300

    Продолжаем верстку

--- source/js/slider.js
+++ source/js/slider.js
@@ -12,32 +12,23 @@ if (progress) {
   btnBefore.addEventListener('click', function () {
     progressWrap.classList.remove('example__progress-wrapper--after');
     progressToggle.classList.remove('example__progress-toggle--after');
-  })
+  });
 
   btnAfter.addEventListener('click', function () {
     progressWrap.classList.add('example__progress-wrapper--after');
     progressToggle.classList.add('example__progress-toggle--after');
   })
-
-
 }
 
-if (document.body.clientWidth>= tabletWidth) {
-    range.addEventListener('input', function() {
-    imgBefore.style.width = (100 - range.value) + '%';
-    imgAfter.style.width = range.value + '%';
-  })
-
+if (innerWidth >= tabletWidth) {
   btnBefore.addEventListener('click', function () {
     imgBefore.style.width = '100%';
     imgAfter.style.width = '0%';
-    range.value = 0;
   })
 
   btnAfter.addEventListener('click', function () {
-    imgAfter.style.width = '100%';
     imgBefore.style.width = '0%';
-    range.value = 100;
+    imgAfter.style.width = '100%';
   })
-
 }
+
