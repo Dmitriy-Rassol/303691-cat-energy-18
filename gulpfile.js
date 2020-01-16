cf0da07292f031bf25d04d5e7cb415a881f03695
Dmitriy-Rassol <rassoldima@yandex.ru>
2020-01-16 11:04:47 +0300

    Завершаем оптимизировать векторную и растровую графику

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var webp = require("gulp-webp");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -33,5 +34,11 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
+
+gulp.task("webp", () =>
+  gulp.src("source/img/*.png")
+    .pipe(webp())
+    .pipe(gulp.dest("source/img"))
+);
 
 gulp.task("start", gulp.series("css", "server"));
